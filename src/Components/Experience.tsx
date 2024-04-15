d9e407df764c6045db406f8bd35700acada1508a
saad-bin-mohammed <94010723+saad-bin-mohammed@users.noreply.github.com>
2024-04-15 11:16:39 +0530

    Rotation using Math

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -1,22 +1,39 @@
-import { OrbitControls } from "@react-three/drei";
+import { MeshDiscardMaterial } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { Mesh } from "three/src/Three.js";
 
 export const Experience = () => {
-  const meshRef = useRef<Mesh>(null);
+  const meshRefSphere = useRef<Mesh>(null);
+  const meshRefTorus = useRef<Mesh>(null);
   useFrame((state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += delta;
+    if (meshRefSphere.current) {
+      meshRefSphere.current.rotation.x += delta;
+      meshRefSphere.current.rotation.y += delta;
+      meshRefSphere.current.position.z = Math.cos(state.clock.elapsedTime);
+    }
+  });
+  useFrame((state, delta) => {
+    if (meshRefTorus.current) {
+      meshRefTorus.current.rotation.y += delta;
+      // meshRefTorus.current.rotation.y += delta;
+      // meshRefTorus.current.position.z = Math.cos(state.clock.elapsedTime);
+      meshRefTorus.current.position.x = Math.cos(state.clock.elapsedTime);
+      // meshRefTorus.current.position.y =
+      //   Math.sin(state.clock.elapsedTime) * 1.16;
     }
   });
   return (
     <>
       {/* <OrbitControls /> */}
-      <ambientLight intensity={1} position={[0, 1, 1]} />
-      <mesh position={[0, 0, 0]} ref={meshRef}>
-        <boxGeometry />
+      {/* <ambientLight intensity={1} position={[0, 1, 0]} /> */}
+      <mesh ref={meshRefSphere}>
+        <sphereGeometry />
         <meshNormalMaterial />
+      </mesh>
+      <mesh ref={meshRefTorus}>
+        <torusGeometry />
+        <meshToonMaterial />
       </mesh>
       {/* <mesh position={[0, 1, 2]}>
         <boxGeometry />
